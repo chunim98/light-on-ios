8be0889f43bdb06780bbcb8359980a362666d44b
chunim98 <jjingeo1230@naver.com>
2025-05-02 18:28:13 +0900

    chore: DIContainer 추가(내용 미구현)

---
+++ LightOn/App/DIContainer.swift
@@ -0,0 +1,14 @@
+//
+//  DIContainer.swift
+//  LightOn
+//
+//  Created by 신정욱 on 5/2/25.
+//
+
+final class DIContainer {
+    
+    // MARK: SingleTone
+    
+    static let shared = DIContainer()
+    init() {}
+}
