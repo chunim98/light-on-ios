81450367d47a990bac7d09ab171554f480770cab
chunim98 <jjingeo1230@naver.com>
2025-06-11 16:11:43 +0900

    refactor: 코디네이터 프로토콜 최신화

--- LightOn/Shared/Coordinator.swift
+++ LightOn/Shared/Coordinator.swift
@@ -8,20 +8,26 @@
 import UIKit
 
 protocol Coordinator: AnyObject {
-    /// weak으로 선언해 줄 것 (이미 부모가 배열에 넣어 강하게 참조 중이기 때문)
+    /// 절대 `weak`으로 선언해 줄 것
+    /// 이미 부모가 배열에 넣어 강하게 참조 중!
     var parent: Coordinator? { get set }
     /// 자식을 배열에 넣어서 강한 참조를 유지
-    var childrens: [Coordinator] { get set }
-    var navigationController : UINavigationController { get }
+    var children: [Coordinator] { get set }
+    var navigation : UINavigationController { get }
     
     func start()
 }
 
 extension Coordinator {
-    /// 자식 코디네이터를 제거 (창을 닫을 때, 꼭 호출해야 함)
-    func childDidFinish(_ coordinator : Coordinator) {
-        childrens.removeAll { $0 === coordinator }
+    
+    /// 자식 코디네이터를 제거
+    func free(child : Coordinator) {
+        children.removeAll { $0 === child }
     }
     
-    func finish() { parent?.childDidFinish(self) }
+    /// 자식 코디네이터를 추가
+    func store(child: Coordinator) {
+        child.parent = self
+        children.append(child)
+    }
 }
