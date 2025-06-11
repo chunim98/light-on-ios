468cb3657f3f15fa9102161255a3c5a73ed33d47
chunim98 <jjingeo1230@naver.com>
2025-06-11 19:18:45 +0900

    feat: 상단 탭바 구현

---
+++ LightOn/Features/PerformanceList/PerformanceListVC.swift
@@ -0,0 +1,7 @@
+//
+//  PerformanceListVC.swift
+//  LightOn
+//
+//  Created by 신정욱 on 6/11/25.
+//
+
---
+++ LightOn/Features/PerformanceList/UpperTabBar.swift
@@ -0,0 +1,174 @@
+//
+//  UpperTabBar.swift
+//  LightOn
+//
+//  Created by 신정욱 on 6/11/25.
+//
+
+import UIKit
+import Combine
+
+import CombineCocoa
+import SnapKit
+
+final class UpperTabBar: UIStackView {
+    
+    // MARK: Properties
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    // MARK: Outputs
+    
+    private let selectedIndexSubject = PassthroughSubject<Int, Never>()
+    
+    // MARK: Components
+
+    // 버튼이 2개 뿐이라, 하드코딩 해버림!
+    private let popularButton = {
+        var textConfig = TextConfiguration()
+        textConfig.letterSpacing = .zero
+        textConfig.alignment = .center
+        textConfig.lineHeight = 18.8
+        textConfig.text = "인기 공연"
+        
+        var config = UIButton.Configuration.plain()
+        config.contentInsets = .init(top: 13, bottom: 8)
+        config.background.backgroundColor = .clear
+        
+        let button = UIButton(configuration: config)
+        button.configurationUpdateHandler = {
+            if $0.isSelected {
+                textConfig.font = .pretendard.bold(17)
+                textConfig.foregroundColor = .brand
+            } else {
+                textConfig.font = .pretendard.regular(17)
+                textConfig.foregroundColor = .assistive
+            }
+            config.attributedTitle = .init(textConfig: textConfig)
+            $0.configuration = config
+        }
+        button.isSelected = true
+        return button
+    }()
+    
+    private let recommendButton = {
+        var textConfig = TextConfiguration()
+        textConfig.letterSpacing = .zero
+        textConfig.alignment = .center
+        textConfig.lineHeight = 18.8
+        textConfig.text = "추천 공연"
+        
+        var config = UIButton.Configuration.plain()
+        config.contentInsets = .init(top: 13, bottom: 8)
+        config.background.backgroundColor = .clear
+
+        let button = UIButton(configuration: config)
+        button.configurationUpdateHandler = {
+            if $0.isSelected {
+                textConfig.font = .pretendard.bold(17)
+                textConfig.foregroundColor = .brand
+            } else {
+                textConfig.font = .pretendard.regular(17)
+                textConfig.foregroundColor = .assistive
+            }
+            config.attributedTitle = .init(textConfig: textConfig)
+            $0.configuration = config
+        }
+        return button
+    }()
+    
+    private let baseIndicator = {
+        let view = Divider(height: 2, color: .background)
+        view.isUserInteractionEnabled = false
+        return view
+    }()
+    
+    private let tintIndicator = {
+        let view = Divider(height: 2, color: .brand)
+        view.isUserInteractionEnabled = false
+        return view
+    }()
+
+    // MARK: Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupDefaults()
+        setupLayout()
+        setupBindings()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Defaults
+    
+    private func setupDefaults() { distribution = .fillEqually }
+    
+    // MARK: Layout
+    
+    private func setupLayout() {
+        addArrangedSubview(popularButton)
+        addArrangedSubview(recommendButton)
+        
+        addSubview(baseIndicator)
+        baseIndicator.addSubview(tintIndicator)
+        
+        self.snp.makeConstraints { $0.height.equalTo(40) }
+        baseIndicator.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+        tintIndicator.snp.makeConstraints {
+            $0.width.equalToSuperview().multipliedBy(0.5)
+            $0.leading.bottom.equalToSuperview()
+        }
+    }
+    
+    // MARK: Bindings
+    
+    private func setupBindings() {
+        let selectedIndex = Publishers.Merge(
+            popularButton.tapPublisher.map { _ in 0 },
+            recommendButton.tapPublisher.map { _ in 1 }
+        ).eraseToAnyPublisher()
+        
+        // 내부 바인딩
+        selectedIndex
+            .sink { [weak self] in self?.selectedIndexBinder(index: $0) }
+            .store(in: &cancellables)
+        
+        // 외부 방출
+        selectedIndex
+            .sink { [weak self] in self?.selectedIndexSubject.send($0) }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: Binders & Publishers
+
+extension UpperTabBar {
+    /// 선택된 버튼에 틴트 효과 적용
+    func selectedIndexBinder(index: Int) {
+        // 버튼 상태 갱신
+        popularButton.isSelected = (index == 0)
+        recommendButton.isSelected = (index == 1)
+        
+        // 인디케이터 바 위치 갱신
+        let inset = (index == 0) ? 0 : bounds.width/2
+        UIView.animate(withDuration: 0.25) { [weak self] in
+            self?.tintIndicator.snp.updateConstraints {
+                $0.leading.equalToSuperview().inset(inset)
+            }
+            self?.baseIndicator.layoutIfNeeded()
+        }
+    }
+    
+    var selectedIndexPublisher: AnyPublisher<Int, Never> {
+        selectedIndexSubject.eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Preview
+
+#Preview { UpperTabBar() }
