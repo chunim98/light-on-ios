35c9dcf78d72b8a53c0f548d07622dc3f070641d
chunim98 <jjingeo1230@naver.com>
2025-07-10 20:53:02 +0900

    chore: 로그인 폼 스타일 로직 수정

--- LightOn/Features/Login/Presentation/LoginForm.swift
+++ LightOn/Features/Login/Presentation/LoginForm.swift
@@ -12,6 +12,17 @@ import CombineCocoa
 
 final class LoginForm: NTextForm {
     
+    // MARK: Struct
+    
+    struct Style {
+        let titleColor: UIColor
+        let borderColor: UIColor
+        
+        static var idle: Self { .init(titleColor: .infoText, borderColor: .thumbLine) }
+        static var focused: Self { .init(titleColor: .brand, borderColor: .brand) }
+        static var filled: Self { .init(titleColor: .loBlack, borderColor: .loBlack) }
+    }
+    
     // MARK: Properties
     
     private var cancellables = Set<AnyCancellable>()
@@ -32,31 +43,36 @@ final class LoginForm: NTextForm {
     
     private func setupDefaults() {
         asteriskLabel.isHidden = true
-        setStyle(phase: .idle)
     }
     
     // MARK: Bindings
     
     private func setupBindings() {
-        Publishers.Merge(
-            textField.didBeginEditingPublisher.map { FormPhase.focused },
-            textField.controlEventPublisher(for: .editingDidEnd).map { FormPhase.filled }
-        )
-        .sink { [weak self] in self?.setStyle(phase: $0) }
-        .store(in: &cancellables)
-    }
-    
-    // MARK: Style
-    
-    override func setStyle(phase: FormPhase) {
-        switch phase {
-        case .idle, .filled, .error:
-            titleLabel.config.foregroundColor = .infoText
-            textField.layer.borderColor = UIColor.thumbLine.cgColor
-            
-        case .focused:
-            titleLabel.config.foregroundColor = .brand
-            textField.layer.borderColor = UIColor.brand.cgColor
-        }
+        let text = textField.textPublisher
+            .compactMap { $0 }
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+        
+        let isFocused = Publishers
+            .Merge(
+                textField.didBeginEditingPublisher.map { true },
+                textField.controlEventPublisher(for: .editingDidEnd).map { false }
+            )
+            .eraseToAnyPublisher()
+        
+        let style = Publishers.CombineLatest(text, isFocused)
+            .map { text, isFocused -> Style in
+                guard !isFocused else { return .focused }
+                return text.isEmpty ? .idle : .filled
+            }
+            .prepend(.idle) // 초기 스타일 할당
+            .eraseToAnyPublisher()
+        
+        style
+            .sink { [weak self] in
+                self?.titleLabel.config.foregroundColor = $0.titleColor
+                self?.textField.layer.borderColor = $0.borderColor.cgColor
+            }
+            .store(in: &cancellables)
     }
 }
